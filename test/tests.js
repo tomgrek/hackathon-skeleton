2a8577ea0c538e82ba7a06a6b3d337731abaabf0
Tom Grek <tom.grek@gmail.com>
2016-08-25 11:04:51 -0700

    Added front-end tests with zombiejs

--- test/tests.js
+++ test/tests.js
@@ -2,17 +2,24 @@ console.log('Running Tests');
 var chai = require('chai'), expect = chai.expect, should = chai.should();
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+var Browser = require('zombie');
+
 var DEV_SERVER = 'localhost:3000';
+
+Browser.localhost('localhost', 3000);
 
 var testFunc = require('../server.js').testFunc;
 var mulFunc = require('../server.js').mulFunc;
 
+// TEST SOME BACK-END FUNCTIONS DIRECTLY
 it('should return 9x10 = 90', function() {
   mulFunc(9,10).should.equal(90);
 });
 it('should return what is passed in', function() {
   testFunc('hello world').should.equal('hello world');
 });
+
+// TEST SOME API CALLS
 it('should successfully serve something', function(done) {
   chai.request(DEV_SERVER)
       .get('/')
@@ -28,4 +35,18 @@ it('should return what is passed in plus ZIG, over http', function(done) {
         res.text.should.equal('abcdefZIG');
         done();
       });
-})
+});
+
+// TEST THE FRONT END
+describe('Visit home page', function() {
+  var browser = new Browser();
+  before(function() { return browser.visit('/'); });
+  it('should serve a page', function(done) {
+    browser.assert.success();
+    done();
+  });
+  it('should load the page with the correct title', function(done) {
+    browser.assert.text('title', 'Bootcamp Skeleton');
+    done();
+  });
+});
