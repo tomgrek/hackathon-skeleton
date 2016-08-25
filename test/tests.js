1cd804a74736b98a5e754f9c35cfcae53c7239d0
Tom Grek <tom.grek@gmail.com>
2016-08-25 09:47:32 -0700

    Updated testing code

--- test/tests.js
+++ test/tests.js
@@ -1,21 +1,15 @@
 console.log('Running Tests');
+var chai = require('chai'), expect = chai.expect, should = chai.should();
 var testfunc = require('../server.js').testfunc;
 var mulFunc = require('../server.js').mulFunc;
 
-//import {testfunc} from '../server.js';
-console.log(testfunc('zddig'));
-console.log(mulFunc(8,8));
-var assert = require('assert');
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
-assert.equal(90, mulFunc(9,10));
-
+/*var assert = require('assert');
 describe('multiply function', it('should return 90', function(done) { assert.equal(90, mulFunc(9,10)); done(); }));
-//var chai =require('chai');
-//chai.expect(mulFunc(9,10)).to.equal(90);
+*/
+
+it('should return 9x10 = 90', function() {
+  mulFunc(9,10).should.equal(90);
+});
+it('should return what is passed in', function() {
+  testfunc('hello world').should.equal('this test deliberately fails');
+});
